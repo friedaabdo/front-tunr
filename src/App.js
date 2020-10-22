ea7598bf43ae87788ce4a75e3bb28953b373ae04
Frieda Abdo <friedabdo@gmail.com>
2020-10-22 12:25:03 -0400

    app and favorites

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, {useState} from "react";
 import "./App.css";
 import Favorites from "./Components/Favorites";
+import {Route, Link, Switch} from 'react-router-dom'
 
 function App() {
  const url = 'http://localhost:3500'
 
  const [songs, setSongs] = useState([])
+
+ const getSongs = () => {
+   fetch(url + '/song/')
+   .then(res => res.json())
+   .then(data => setSongs(data))
+ }
 
 
 
@@ -13,7 +20,8 @@ function App() {
     <div className="App">
       <h1>Tunr.</h1>
       <h2>For all your playlist needs</h2>
-      <Favorites />
+    <Route path='/favorites'><Favorites /></Route>
+      
     </div>
   );
 }
--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Favorites = () => {
+const Favorites = (props) => {
+ //some state here to grab favorites
+ const [faves, setFaves] = React.useState([])
+
     return(
-        <h3>Favorite Song List</h3>
+        <div>
+         <h3>Favorite Song List</h3>
+            <>{faves}</>
+        </div>
     )
 }
 
