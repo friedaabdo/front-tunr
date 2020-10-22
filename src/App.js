d636fa526d83a87ef2cf026c24f3c04161c47dfa
Frieda Abdo <friedabdo@gmail.com>
2020-10-22 12:45:10 -0400

    style from here

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import "./App.css";
 import Favorites from "./Components/Favorites";
 import {Route, Link, Switch} from 'react-router-dom'
+import Form from "./Components/Form"
 
 function App() {
  const url = 'http://localhost:3500'
@@ -14,15 +15,19 @@ function App() {
    .then(data => setSongs(data))
  }
 
-
+const emptySong = {
+  Song: '',
+  Artist: '',
+  Time: ''
+}
 
   return (
 
     <div className="App">
       <h1>Tunr.</h1>
       <h2>For all your playlist needs</h2>
+      <Form emptySong={emptySong}/>
     <Route path='/favorites'><Favorites /></Route>
-      
     </div>
   );
 
