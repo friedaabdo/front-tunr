be41c1b9a9ab4b30165f2d2d81e7dc0dc3698e50
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-22 15:39:29 -0400

    added styling to playlist and add some songs added id's to text elements in Playlist and App

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -1,29 +1,34 @@
 //import React
-import React from 'react';
+import React from "react";
 
 //Create Playlist Component
 const Playlist = (props) => {
-	const playlistSongs = () => (
-		<div id='song-list'>
-			{props.songs.map((song) => (
-				<article>
-					<section className='song-title'>{song.title}</section> 
-					<section>{song.time}</section> 
-					<section>{song.artist}</section> 
-					<section
-						className='delete'
-						onClick={() => {
-							props.removeSong(song);
-							props.history.push('/delete');
-						}}>
-						X
-					</section>
-				</article>
-            ))}
-		</div>
-    );
+  const playlistSongs = () => (
+    <div id="song-list">
+      {props.songs.map((song) => (
+        <article>
+          <section className="song-title">{song.title}</section>
+          <section>{song.time}</section>
+          <section>{song.artist}</section>
+          <section
+            className="delete"
+            onClick={() => {
+              props.removeSong(song);
+              props.history.push("/delete");
+            }}
+          >
+            X
+          </section>
+        </article>
+      ))}
+    </div>
+  );
 
-	return props.songs.length > 0 ? playlistSongs() : <h1>Add Some Songs!</h1>;
+  return props.songs.length > 0 ? (
+    playlistSongs()
+  ) : (
+    <h1 id="addSomeSongs">Add Some Songs!</h1>
+  );
 };
 
 export default Playlist;
