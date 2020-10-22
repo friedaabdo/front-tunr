19ba7f7b2b5be7a175b6e4c072527ac20b44b545
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-10-22 12:08:40 -0400

    feature-form - added form functionality

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,25 +1,55 @@
+//Import React
 import React from 'react';
 
+//Create Form Component
 const Form = (props) => {
-    const handleSubmit = 
-    
-    return (
+	//state for formData
+	const [formData, setFormData] = React.useState();
+
+	//handleSubmit function to lift data to App
+	const handleSubmit = (event) => {
+		event.preventDefault(); //Prevent Form from Refreshing
+		props.handleSubmit(formData); //Submit to App's desired function
+		props.history.push('/'); //Push back up to display section
+	};
+
+	//HandleChange function to set formData to input from form
+	const handleChange = (event) => {
+		setFormData({ ...formData, [event.target.name]: event.target.value });
+	};
+	return (
 		<form>
 			<label for='formTitle'>Title</label>
 			<br />
-			<input type='text' id='form-title' name='formTitle' />
+			<input
+				type='text'
+				id='form-title'
+				name='formTitle'
+				value={formData.title}
+				onChange={handleChange}
+			/>
 			<br />
 			<label for='formArtist'>Artist</label>
 			<br />
-			<input type='text' id='form-artist' name='formArtist' />
+			<input
+				type='text'
+				id='form-artist'
+				name='formArtist'
+				value={formData.artist}
+				onChange={handleChange}
+			/>
 			<br />
 			<label for='formTime'>Time</label>
 			<br />
-			<input type='text' id='form-title' name='formTime' />
+			<input
+				type='text'
+				id='form-title'
+				name='formTime'
+				value={formData.title}
+				onChange={handleChange}
+			/>
 			<br />
-			<input type='submit' value='submit'>
-				Add New Song
-			</input>
+			<input type='submit' value='Add New Song' />
 		</form>
 	);
 };
